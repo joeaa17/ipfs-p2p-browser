4917c0f3e62322bc45277d8745bacbc5cc6ef87d
joeaa17 <32424114+joeaa17@users.noreply.github.com>
2022-12-22 21:33:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -98,4 +98,235 @@ document.addEventListener('DOMContentLoaded', async () => {
   // Export libp2p to the window so you can play with the API
   window.libp2p = libp2p
 
+  let count = 0
+  let segment = [];
+  let myChart = null
+  let myChartLine = null
+  let myChartTime = null
+  let peerPrevious = 0
+  let peerPreviousArray = []
+  let dialPrevious = 0
+  let dialPreviousArray = []
+  let connectedPrevious = 0
+  let connectedPreviousArray = []
+  let disconnectedPrevious = 0
+  let disconnectedPreviousArray = []
+      
+  function loopGraph () {
+    setTimeout(() => {
+      const peerC = document.getElementById('peers').innerText
+      const dialC = document.getElementById('dial').innerText
+      const connectedC = document.getElementById('connected').innerText
+      const disconnectedC = document.getElementById('disconnected').innerText
+
+      const data = {
+        labels: ['Peers', 'Dial', 'Connected', 'Disconnected'],
+        datasets: [{
+          label: 'Libp2p',
+          data: [peerC, dialC, connectedC, disconnectedC],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ],
+
+          borderWidth: 1
+        }]
+      }
+
+      const config = {
+        type: 'bar',
+        data,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      }
+
+      if (myChart) {
+        myChart.config = config
+        myChart.update()
+      }
+      else {
+        myChart = new Chart(
+          document.getElementById('chartBar'),
+          config
+        )
+      }
+
+      // chartLine
+      const dataLine = {
+        labels: ['Peers', 'Dial', 'Connected', 'Disconnected'],
+        datasets: [{
+          label: 'Libp2p',
+          data: [peerC - peerPrevious, dialC - dialPrevious, connectedC - connectedPrevious, disconnectedC - disconnectedPrevious],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ],
+
+          borderWidth: 1
+        }]
+
+      }
+
+      const configLine = {
+        type: 'line',
+        data: dataLine,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      }
+
+      if (myChartLine) {
+        myChartLine.config = configLine
+        myChartLine.update()
+      }
+      else {
+        myChartLine = new Chart(
+          document.getElementById('chartLine'),
+          configLine
+        )
+      }
+
+      peerPrevious = peerC
+      dialPrevious = dialC
+      connectedPrevious = connectedC
+      disconnectedPrevious = disconnectedC
+
+      peerPreviousArray.push(peerC)
+      dialPreviousArray.push(dialC)
+      connectedPreviousArray.push(connectedC)
+      disconnectedPreviousArray.push(disconnectedC)
+
+      // chartTime
+      const dataTime = {
+        labels: segment,
+        datasets: [{
+          label: 'peers',
+          data: peerPreviousArray,
+          backgroundColor: [
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(255, 255, 255, 0.2)'
+
+          ],
+          borderColor: [
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(255, 255, 255, 0.2)'
+          ],
+
+          borderWidth: 1
+        },
+        {
+          label: 'dial',
+          data: dialPreviousArray,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ],
+
+          borderWidth: 1
+        },
+        {
+          label: 'connected',
+          data: connectedPreviousArray,
+          backgroundColor: [
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          borderColor: [
+            'rgba(75, 192, 192, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(75, 192, 192, 1)'
+          ],
+
+          borderWidth: 1
+        },
+        {
+          label: 'disconnected',
+          data: disconnectedPreviousArray,
+          backgroundColor: [
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(54, 162, 235, 0.2)'
+          ],
+          borderColor: [
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(54, 162, 235, 0.2)'
+          ],
+
+          borderWidth: 1
+        }]
+      }
+
+      const configTime = {
+        type: 'line',
+        data: dataTime,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      }
+
+      if (myChartTime) {
+        myChartTime.config = configTime
+        myChartTime.update()
+      }
+      else {
+        myChartTime = new Chart(
+          document.getElementById('chartTime'),
+          configTime
+        )
+      }
+
+      segment.push(++count)
+
+      loopGraph()
+    }, 4000)
+  }
+    loopGraph()
+
 })
